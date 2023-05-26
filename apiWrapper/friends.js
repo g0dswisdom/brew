5fcddf30127a2a5538e8d268d8d6eaf2f7149ba5
g0dswisdom <128635829+g0dswisdom@users.noreply.github.com>
2023-05-26 11:37:59 +0300

    Update friends.js

--- apiWrapper/friends.js
+++ apiWrapper/friends.js
@@ -1,28 +1,19 @@
-const myFriendCount = require("./friends/myFriendCount")
-const pendingRequests = require("./friends/pendingRequests")
-const userFriendCount = require("./friends/userFriendCount")
-const sendFriendRequest = require("./friends/sendFriendRequest")
-const declineAll = require("./friends/declineAll")
-const userFollowerCount = require("./friends/userFriendCount")
-const acceptFriendRequest = require("./friends/acceptFriendRequest")
-const declineFriendRequest = require("./friends/declineFriendRequest")
-const acceptFriendRequestWithToken = require("./friends/acceptReqWithToken")
-const unfriendUser = require("./friends/unfriendUser")
+const { readdirSync } = require('fs')
+const { join } = require('path')
 
-const friends = {
-    async myFriendCount() { return myFriendCount.myFriendCount() },
-    async pendingRequests() { return pendingRequests.pendingRequests() },
-    async userFriendCount(id) { return userFriendCount.userFriendCount(id) },
-    async sendFriendRequest(id) { return sendFriendRequest.sendFriendRequest(id) },
-    async declineAll() { return declineAll.declineAll() },
-    async userFollowerCount(id) { return userFollowerCount.userFriendCount(id) },
-    async acceptFriendRequest(id) { return acceptFriendRequest.acceptFriendRequest(id) },
-    async declineFriendRequest(id) { return declineFriendRequest.declineFriendRequest(id) },
-    async acceptFriendRequestWithToken(id, friendingToken) { return acceptFriendRequestWithToken.acceptFriendRequestWithToken(id, friendingToken) },
-    async unfriendUser(id) { return unfriendUser.unfriendUser(id) }
+const friendsDir = join(__dirname, 'friends')
+
+const brewFriends = readdirSync(friendsDir)
+  .filter(file => file.endsWith('.js'))
+  .reduce((acc, file) => {
+    const { name, ext } = parseFileName(file)
+    acc[name] = require(join(friendsDir, file))[name]
+    return acc
+  }, {})
+
+function parseFileName(file) {
+  const [name, ext] = file.split('.')
+  return { name, ext }
 }
 
-
-module.exports = {
-    brewFriends: friends,
-}
+module.exports = { brewFriends }
